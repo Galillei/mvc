3f1c0a9856f4f438c4e1d906e0b21d463ee50a3b
Galillei <bazilio.rodger@gmail.com>
2013-10-31 21:31:19 +0300

    entityManager which really work

---
+++ entityManager.php
@@ -0,0 +1,40 @@
+<?php
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+require_once "vendor/autoload.php";
+$dbParams = array(
+    'driver'   => 'pdo_mysql',
+    'user'     => 'root',
+    'password' => 'bazilio',
+    'dbname'   => 'books',
+);
+$argument = array();
+$paths = array(__DIR__."/Model/Entity/");
+$isDevMode = true;
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
+//$classes = $entityManager->getClassMetadataFactory()->getAllMetadata();
+//$generator = new \Doctrine\ORM\Tools\EntityGenerator();
+//$generator->setGenerateAnnotations(true);
+//$generator->setGenerateStubMethods(true);
+//$generator->setRegenerateEntityIfExists(false);
+//$generator->setUpdateEntityIfExists(true);
+//$generator->generate($classes, 'Model/Entity/');
+$entityManager->getConfiguration()->setMetadataDriverImpl(new \Doctrine\ORM\Mapping\Driver\DatabaseDriver(
+    $entityManager->getConnection()->getSchemaManager()
+)
+);
+
+$cmf = new \Doctrine\ORM\Tools\DisconnectedClassMetadataFactory();
+$cmf->setEntityManager($entityManager);
+$metadata = $cmf->getAllMetadata();
+$cme = new \Doctrine\ORM\Tools\Export\ClassMetadataExporter();
+$generator = new \Doctrine\ORM\Tools\EntityGenerator();
+$generator->setGenerateAnnotations(true);
+$generator->setGenerateStubMethods(true);
+$generator->setRegenerateEntityIfExists(false);
+$generator->setUpdateEntityIfExists(true);
+$generator->generate($metadata, 'Model/Entity');
+//$exporter = $cme->getExporter('annotation','Model/Entity');
+//$exporter->setMetadata($metadata);
+//$exporter->export();
