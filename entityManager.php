e8499605701d95af523f92a83c61a7878b121fd2
Galillei <bazilio.rodger@gmail.com>
2013-10-31 21:57:39 +0300

    entityManager which really work

--- entityManager.php
+++ entityManager.php
@@ -13,18 +13,10 @@ $paths = array(__DIR__."/Model/Entity/");
 $isDevMode = true;
 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
 $entityManager = EntityManager::create($dbParams, $config);
-//$classes = $entityManager->getClassMetadataFactory()->getAllMetadata();
-//$generator = new \Doctrine\ORM\Tools\EntityGenerator();
-//$generator->setGenerateAnnotations(true);
-//$generator->setGenerateStubMethods(true);
-//$generator->setRegenerateEntityIfExists(false);
-//$generator->setUpdateEntityIfExists(true);
-//$generator->generate($classes, 'Model/Entity/');
 $entityManager->getConfiguration()->setMetadataDriverImpl(new \Doctrine\ORM\Mapping\Driver\DatabaseDriver(
     $entityManager->getConnection()->getSchemaManager()
 )
 );
-
 $cmf = new \Doctrine\ORM\Tools\DisconnectedClassMetadataFactory();
 $cmf->setEntityManager($entityManager);
 $metadata = $cmf->getAllMetadata();
